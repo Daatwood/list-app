11fab947fa61b6afd5783d817a21d26810bfb588
Dustin Atwood <dustin@atwood.email>
2018-05-03 12:04:55 -0500

    added fallback route

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
       resources :lists
     end
   end
+  get '*path', to: "application#fallback", constraints: ->(request) do
+    !request.xhr? && request.format.html?
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
